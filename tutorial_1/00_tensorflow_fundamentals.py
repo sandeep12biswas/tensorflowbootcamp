96791844980c8c5b432fdf0a19070a703bceaf8d
Sandeep Biswas <sandeep12.biswas@gmail.com>
2025-06-27 13:08:26 +0530

    added scaler. vector and matrices with ndim

--- tutorial_1/00_tensorflow_fundamentals.py
+++ tutorial_1/00_tensorflow_fundamentals.py
@@ -1,2 +1,34 @@
 import tensorflow as tf
+from numpy.matrixlib.defmatrix import matrix
+
+# check the tensor version
+
+version = tf.__version__
+print(version)
+
+# create tensor with tf.constant()
+scaler = tf.constant(7)
+print(scaler)
+
+#Check the number of dimensions of tenson (ndim stands for number of dimensions)
+
+print(scaler.ndim)
+
+#vectos constants
+
+vector = tf.constant([10,10])
+print(vector)
+
+print(vector.ndim)
+
+matrix = tf.constant([[7,10],
+                     [10,7]])
+print(matrix)
+print(matrix.ndim)
+
+another_matrix = tf.constant([[10.,7.],
+                              [11.,5.],
+                              [5.,5.]], dtype=tf.float16)
+print(another_matrix)
+print(another_matrix.ndim)
 
